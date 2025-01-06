27ede01f4f194f22e4d6568c6b552f00d88e5535
binhan <qweqazhank123@gmail.com>
2025-01-06 12:23:09 +0800

    處理 story 後端

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
+from api.langchain.story import router as story_router
+
 api_router = APIRouter(
     prefix="/api",
 )
 
+api_router.include_router(story_router, prefix="/langchain", tags=["LangChain"])
 
 __all__ = ["api_router"]
